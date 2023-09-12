4dda4e0a25e571873edd7b183f8481a8b07f7a47
Petr Tsvetkov <petr.tsv@gmail.com>
2023-09-13 00:43:53 +0100

    Implement basic method counter

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,9 +15,7 @@ repositories {
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
   version.set("2022.2.5")
-  type.set("IC") // Target IDE Platform
-
-  plugins.set(listOf(/* Plugin Dependencies */))
+  plugins.set(listOf("org.jetbrains.kotlin"))
 }
 
 tasks {
---
+++ src/main/kotlin/org/petrtsv/methodscounterplugin/CountClassMethods.kt
@@ -0,0 +1,32 @@
+package org.petrtsv.methodscounterplugin
+
+import com.intellij.codeInsight.intention.IntentionAction
+import com.intellij.codeInsight.intention.PsiElementBaseIntentionAction
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.Messages
+import com.intellij.psi.PsiElement
+import com.intellij.util.castSafelyTo
+import org.jetbrains.kotlin.psi.KtClass
+
+class CountClassMethods : PsiElementBaseIntentionAction(), IntentionAction {
+    override fun getFamilyName() = "Count methods in a class"
+
+    override fun getText() = getFamilyName()
+
+    override fun isAvailable(project: Project, editor: Editor?, element: PsiElement): Boolean {
+        // user is pointing to a class name
+        return element.parent.castSafelyTo<KtClass>()?.let { it.nameIdentifier == element } ?: false
+    }
+
+    override fun invoke(project: Project, editor: Editor?, element: PsiElement) {
+        element.parent.castSafelyTo<KtClass>()?.let { classElement ->
+            // we assume than the name exists for the class as we're pointing at it
+            val className = classElement.name
+            val dialogTitle = "Counting methods for class \"$className\""
+            val dialogMessage = "Number of methods: ${classElement.body?.functions?.size}"
+
+            Messages.showMessageDialog(project, dialogMessage, dialogTitle, Messages.getInformationIcon())
+        }
+    }
+}
